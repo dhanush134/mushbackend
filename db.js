4f5881bd92b8d6365526aa554a21a3d686dd8dca
rake3897 <131149368+rake3897@users.noreply.github.com>
2026-01-31 18:12:08 +0530

    Initial commit2

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.postgresql://postgres:uHCjCcuJLOqvwMePvQUhvRghhIAvmduY@postgres.railway.internal:5432/railway
+  connectionString: process.env.postgres:uHCjCcuJLOqvwMePvQUhvRghhIAvmduY@postgres.railway.internal:5432/railway
 });
